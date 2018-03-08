ad63036d94b3ff98d5a3c98d1dcc53b51044d59b
pplou <lousilva@gmail.com>
2018-03-08 23:30:32 +0000

    First Burgui

---
+++ src/containers/BurguerBuilder/BurguerBuilder.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+
+import Aux from '../../hoc/Auxiliary';
+import Burguer from '../../components/Burguer/Burguer';
+
+class BurguerBuilder extends Component {
+    render() {
+        return(
+            <Aux>
+                <Burguer />
+                <div>Build Controls</div>
+            </Aux>
+        );
+    }
+}
+
+export default BurguerBuilder;
