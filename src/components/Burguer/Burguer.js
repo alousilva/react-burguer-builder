b8236ca96dd35167684049946e65e44967373e21
pplou <lousilva@gmail.com>
2018-03-10 17:17:22 +0000

    Burguer Builder with Price and Modal

--- src/components/Burguer/Burguer.js
+++ src/components/Burguer/Burguer.js
@@ -3,11 +3,23 @@ import classes from "./Burguer.css";
 import BurguerIngredient from "./BurguerIngredient/BurguerIngredient";
 
 const burguer = props => {
+    let transformedIngredients = Object.keys(props.ingredients)
+        .map(igKey => {
+            return [...Array(props.ingredients[igKey])].map((_, i) => {
+                return <BurguerIngredient key={igKey + i} type={igKey} />;
+            });
+        })
+        .reduce((arr, el) => {
+            return arr.concat(el)
+        }, []);
+    console.log("ingredients: ", transformedIngredients, transformedIngredients.length);
+    if(transformedIngredients.length === 0){
+        transformedIngredients = <p>Please start adding ingredients!</p>;
+    }
     return (
         <div className={classes.Burguer}>
             <BurguerIngredient type="bread-top" />
-            <BurguerIngredient type="cheese" />
-            <BurguerIngredient type="meat" />
+            {transformedIngredients}
             <BurguerIngredient type="bread-bottom" />
         </div>
     );
