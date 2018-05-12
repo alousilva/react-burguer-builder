91745ed8bb2c88b9de591a6be90c7c4d28e43d73
pplou <lousilva@gmail.com>
2018-05-12 16:59:28 +0100

    With SideDrawer

--- src/components/Burguer/OrderSummary/OrderSummary.js
+++ src/components/Burguer/OrderSummary/OrderSummary.js
@@ -1,30 +1,39 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-import Aux from '../../../hoc/Auxiliary';
+import Aux from '../../../hoc/Auxiliary/Auxiliary';
 import Button from '../../UI/Button/Button';
 
-const orderSummary = (props) => {
-    const ingredientSummary = Object.keys(props.ingredients)
+class OrderSummary extends Component {
+    //This can be a functional component. It's statefull component just for debuggin purposes
+    componentWillUpdate() {
+        console.log('[OrderSummary] WillUpdate');
+    }
+
+    render() {
+        const ingredientSummary = Object.keys(this.props.ingredients)
         .map(igKey => {
-        return (
-            <li key={igKey}>
-                <span style={{textTransform: 'capitalize'}}>{igKey}</span>: {props.ingredients[igKey]}
-            </li>);
+            return (
+                <li key={igKey}>
+                    <span style={{textTransform: 'capitalize'}}>{igKey}</span>: {this.props.ingredients[igKey]}
+                </li>);
         });
-    console.log("ingredientSummary:", ingredientSummary);
-    return (
-        <Aux>
+        
+        console.log("ingredientSummary:", ingredientSummary);
+        
+        return (
+            <Aux>
             <h3>Your Order</h3>
             <p>A delicious Xis-Burgui with the following ingredients:</p>
             <ul>
                 {ingredientSummary}
             </ul>
-            <p><strong>Total Price: {props.totalPrice.toFixed(2)} €</strong></p>
+            <p><strong>Total Price: {this.props.totalPrice.toFixed(2)} €</strong></p>
             <p>Continue to Checkout?</p>
-            <Button btnType='Danger' clicked={props.purchaseCanceled}>CANCEL</Button>
-            <Button btnType='Success' clicked={props.purchaseContinued}>CONTINUE</Button>
+            <Button btnType='Danger' clicked={this.props.purchaseCanceled}>CANCEL</Button>
+            <Button btnType='Success' clicked={this.props.purchaseContinued}>CONTINUE</Button>
         </Aux>
-    );
-};
+        );
+    }
+}
 
-export default orderSummary;
+export default OrderSummary;
